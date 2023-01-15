f244b7ff6f491fffbcc772589382471875b1fbce
D <kritantablake@gmail.com>
2023-01-15 20:25:31 +0300

    add WcfServiceUI

---
+++ hw2/WcfService/WcfServiceUI/SimpleUI.cs
@@ -0,0 +1,22 @@
+﻿using ConsoleApp1.WcfServiceReference1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WcfServiceUI
+{
+    public class SimpleUI : IService1Callback
+    {
+        public void UpdateStatistics(StatisticsService statisticsService)
+        {
+            Console.Clear();
+            Console.CursorVisible = false;
+            Console.WriteLine($"update scripts statistics: {statisticsService.GetHashCode()}");
+            Console.WriteLine($"Total: {statisticsService.TotalCount}");
+            Console.WriteLine($"Success: {statisticsService.SuccessCount}");
+            Console.WriteLine($"Fail: {statisticsService.FailCount}");
+        }
+    }
+}
