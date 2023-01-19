3a94f0d043953e6a5613df06cd51d776806e0ee0
D <kritantablake@gmail.com>
2023-01-19 21:04:39 +0300

    add vetclinicservice

---
+++ hw3/VetClinicService/Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VetClinicService;
+using VetClinicService.Library.DBContext;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddGrpc();
+
+builder.Services.AddDbContext<VetClinicDbContext>(opt =>
+{
+    opt.UseSqlite("Data Source=VCS.db;", b => b.MigrationsAssembly("VetClinicService"));
+});
+
+var app = builder.Build();
+
+app.MapGrpcService<VetClinicService.Services.VetService>();
+
+app.Run();
+
---
+++ hw3/VetClinicService/Services/VetService.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using Grpc.Core;
+using VetClinicService.Library.DBContext;
+using VetClinicService.Library.Models;
+
+namespace VetClinicService.Services
+{
+	public class VetService : VetClinicService.VetClinicServiceBase
+	{
+        private readonly VetClinicDbContext _dbContext;
+
+
+
+        public VetService(VetClinicDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+
+
+        public override Task<CreateClientResponse> CreateClient(CreateClientRequest request, ServerCallContext context)
+        {
+            try
+            {
+                var client = new Client
+                {
+                    Document = request.Document,
+                    Lastname = request.Lastname,
+                    Firstname = request.Firstname,
+                    Patronymic = request.Patronymic
+                };
+
+                _dbContext.Clients.Add(client);
+                _dbContext.SaveChangesAsync();
+
+                var response = new CreateClientResponse
+                {
+                    ClientId = client.Id,
+                    ErrorCode = 0,
+                    ErrorMessage = ""
+                };
+
+                return Task.FromResult(response);
+            }
+            catch(Exception ex)
+            {
+                var response = new CreateClientResponse
+                {
+                    ErrorCode = 1,
+                    ErrorMessage = $"{ex.Message}"
+                };
+
+                return Task.FromResult(response);
+            }
+        }
+
+        public override Task<GetClientsResponse> GetClients(GetClientsRequest request, ServerCallContext context)
+        {
+            try
+            {
+                var response = new GetClientsResponse();
+
+                var clients = _dbContext.Clients.Select(x => new ClientResponse
+                {
+                    ClientId = x.Id,
+                    Document = x.Document,
+                    Firstname = x.Firstname,
+                    Lastname = x.Lastname,
+                    Patronymic = x.Patronymic
+                }).ToList();
+
+                response.Clients.AddRange(clients);
+
+                return Task.FromResult(response);
+            }
+            catch(Exception ex)
+            {
+                var response = new GetClientsResponse
+                {
+                    ErrorCode = 1,
+                    ErrorMessage = $"{ex.Message}"
+                };
+
+                return Task.FromResult(response);
+            }
+        }
+    }
+}
+
