59c90a42335dab640200ed1fb92cd676f94300cc
D <kritantablake@gmail.com>
2023-01-19 21:05:01 +0300

    add vetclinic.library

---
+++ hw3/VetClinicService.Library/DbContext/VetClinicDbContext.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Diagnostics;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using VetClinicService.Library.Models;
+
+namespace VetClinicService.Library.DBContext
+{
+	public class VetClinicDbContext : DbContext
+	{
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<Pet> Pets { get; set; }
+        public DbSet<Consultation> Consultations { get; set; }
+
+
+
+        public VetClinicDbContext(DbContextOptions options) : base(options)
+		{
+            //Database.EnsureDeleted();
+            //Database.EnsureCreated();
+        }
+
+
+
+        #region Creating connection
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                .UseSqlite("Data Source=VCS.db;");
+        }
+
+        #endregion
+
+
+        #region Creating tables
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Client>();
+            modelBuilder.Entity<Pet>();
+            modelBuilder.Entity<Consultation>().HasOne(x => x.Pet).WithMany(y => y.Consultations).HasForeignKey(k => k.PetId).OnDelete(DeleteBehavior.NoAction);
+        }
+
+        #endregion
+    }
+}
+
---
+++ hw3/VetClinicService.Library/Models/Pet.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace VetClinicService.Library.Models
+{
+    [Table("Pets", Schema = "Consumer")]
+    public class Pet
+	{
+        [Required]
+        [Column("id")]
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [Column("master_id")]
+        [ForeignKey(nameof(Master))]
+        public int MaseterId { get; set; }
+
+        [Column("moniker")]
+        [MaxLength(20)]
+        public string Moniker { get; set; }
+
+        [Column("birthday")]
+        public DateTime Birthday { get; set; }
+
+        [Column("master_link")]
+        public Client Master { get; set; }  //хозяин же у питомца, а не клиент
+
+        [Column("consultations")]
+        [InverseProperty(nameof(Consultation.Pet))]
+        public ICollection<Consultation> Consultations { get; set; } = new HashSet<Consultation>();
+    }
+}
+
